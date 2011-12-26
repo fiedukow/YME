4b48350bf285922a342c155aebbf7e85fd607ce3
Andrzej 'fiedukow' Fiedukowicz <fiedukow@gmail.com>
2011-12-26 14:54:37 +0100

    Zarys klas modelu i kontrolera

---
+++ src/YME.java
@@ -0,0 +1,11 @@
+import controller.Controller;
+import model.Model;
+
+/*Main class*/
+public class YME {
+	public static final void main( String[] args )
+	{		
+		System.out.println("Witaj w YME :-)");
+		new Controller(new Model());
+	}
+}
