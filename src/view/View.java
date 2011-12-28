cfe5b380a2024520291c681e0fb016f11454c66c
Andrzej 'fiedukow' Fiedukowicz <fiedukow@gmail.com>
2011-12-28 20:31:12 +0100

    Dlaszy ciag prob z interfacem. Pora na refleksje nad komunikacja Model -> Kontroler -> Widok :-(

--- src/view/View.java
+++ src/view/View.java
@@ -3,17 +3,21 @@ package view;
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridLayout;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.border.TitledBorder;
 
@@ -25,6 +29,7 @@ public class View {
 	JPanel attributes;
 	JPanel statusBar;
 	JTextArea statusText;
+	JComboBox objectSelected;
 	public View()
 	{		
 		mainFrame 	 = new JFrame("YME :: new map");
@@ -34,6 +39,8 @@ public class View {
 		attributes 	 = new JPanel();
 		statusBar 	 = new JPanel();
 		Container cp = mainFrame.getContentPane();
+		
+		mainFrame.setPreferredSize(new Dimension(1024,768));
 		
 		JMenuBar menuBar = new JMenuBar();
 
@@ -58,7 +65,7 @@ public class View {
 		
 		
 		
-		statusText = new JTextArea(5,100);		
+		statusText = new JTextArea("Debug window:\n",5,100);		
 		statusText.setLineWrap(true);
 		JScrollPane statusTextScroll = new JScrollPane(statusText);
 		statusTextScroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
@@ -101,13 +108,32 @@ public class View {
 		toolbox.add(ButtonEllipse);
 		toolbox.add(ButtonQuey);
 		
+		JButton buttonPrev = new JButton("<");
+		JButton buttonNext = new JButton(">");
 		
-		//attributes.setLayout(new GridLayout(2,2));
-		attributes.setPreferredSize( new Dimension(192,512) );
-		attributes.add(new JButton("K"));
-		attributes.add(new JButton("K"));
-		attributes.add(new JButton("Kotek"));
-		attributes.add(new JButton("Kotek"));
+		objectSelected = new JComboBox();	
+		objectSelected.setFont( new Font("Courier", Font.PLAIN, 12) );
+		objectSelected.addItem("Cała mapa");
+		objectSelected.addItem("Start łodzi");
+		objectSelected.addItem("Polygon   #1");
+		objectSelected.addItem("Rectangle #2");
+		objectSelected.addItem("Ellipse   #3");
+		attributes.add(buttonPrev);
+		attributes.add(objectSelected);
+		attributes.add(buttonNext);
+		attributes.setPreferredSize(new Dimension(300, 400));
+		
+		
+		JLabel textureNameLabel = new JLabel("Tekstura: ");
+		JTextField textureName = new JTextField(10);
+		JButton textureFind = new JButton("...");
+		textureFind.setPreferredSize(new Dimension(30, 18));
+		attributes.add(textureNameLabel);
+		attributes.add(textureName);
+		attributes.add(textureFind);
+		
+		
+		
 		
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainFrame.pack();
